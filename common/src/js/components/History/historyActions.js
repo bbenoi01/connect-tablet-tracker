948ccd6a7ef24a98feca513f28d03ce88c6f7003
Brandon <b.nutty@gmail.com>
2018-09-03 15:18:42 -0700

    removed dup function

--- common/src/js/components/History/historyActions.js
+++ common/src/js/components/History/historyActions.js
@@ -14,13 +14,3 @@ export function addNewHistoryItem(name, device) {
                     .catch(err => alert('Error, checkout unsuccessful'))
     };
 }
-
-// export function addNewHistoryItem(name, device, date, time) {
-//     return (dispatch) => {
-//         axios.post('http://localhost:3000/api/historyItems', { name, device, date, time })
-//         .then(res => {
-//             dispatch(findHistoryItems())
-//         })
-//         .catch(err => alert('Error, checkout unsuccessful'))
-//     }
-// }
